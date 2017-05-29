b19c372450ed4703bcdd4984ed0ff39ee5c04ae9
Marcelo Mogrovejo <mmogrovejo@mmogrovejo.local>
2017-05-29 18:02:51 -0300

    Started to Add storyboard components

---
+++ HomeReport/HomeReport/HomeListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeListViewController.swift
+//  HomeReport
+//
+//  Created by Marcelo Mogrovejo on 5/29/17.
+//  Copyright © 2017 Marcelo Mogrovejo. All rights reserved.
+//
+
+import UIKit
+
+class HomeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
