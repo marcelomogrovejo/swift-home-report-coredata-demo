5a59d11a991eb72d880ff57b5ffe6c84c34ba50d
Marcelo Mogrovejo <mmogrovejo@mmogrovejo.local>
2017-05-30 18:04:29 -0300

    Started lecture 026 - Aggregate function

--- HomeReport/HomeReport/Home+CoreDataClass.swift
+++ HomeReport/HomeReport/Home+CoreDataClass.swift
@@ -12,6 +12,9 @@ import CoreData
 @objc(Home)
 public class Home: NSManagedObject {
 
+    var soldPredicate: NSPredicate = NSPredicate(format: "isForSale = false")
+    let request: NSFetchRequest<Home> = Home.fetchRequest()
+    
     func getHomesByStatus(request: NSFetchRequest<Home>, moc: NSManagedObjectContext) -> [Home] {
         do {
             let homes = try moc.fetch(request)
@@ -19,6 +22,28 @@ public class Home: NSManagedObject {
         } catch {
             fatalError("Error in getting list of homes")
         }
-        
     }
+    
+    internal func getTotalHomeSales(moc: NSManagedObjectContext) -> String {
+        request.predicate = soldPredicate
+        request.resultType = .dictionaryResultType
+        
+        let sumExpressionDescription = NSExpressionDescription()
+        sumExpressionDescription.name = "totalSales"
+        sumExpressionDescription.expression = NSExpression(forFunction: "sum", arguments: [NSExpression(forKeyPath: "price")])
+        sumExpressionDescription.expressionResultType = .doubleAttributeType
+        
+        request.propertiesToFetch = [sumExpressionDescription]
+        
+        do {
+            let results = try moc.fetch(request as! NSFetchRequest<NSFetchRequestResult>) as! [NSDictionary]
+            let dictionary = results.first!
+            let totalSales = dictionary["totalSales"] as! Double
+            
+            return totalSales.currencyFormatter
+        } catch {
+            fatalError("Error getting total home sales")
+        }
+    }
+    
 }
